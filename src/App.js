b77b19cee7d627ea4f839d3330359d470ef95f94
Ivo <ivokalendar@icloud.com>
2020-09-17 13:13:46 +0200

    Basic Links

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello from my-react-app5</h1>
-    </div>
-  );
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+)
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+)
+
+const Topics = () => (
+  <div>
+    <h2>Topics</h2>
+  </div>
+)
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <ul>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/about'>About</Link></li>
+            <li><Link to='/topics'>Topics</Link></li>
+          </ul>
+
+          <hr />
+
+          <Route exact path='/' component={Home} />
+          <Route path='/about' component={About} />
+          <Route path='/topics' component={Topics} />
+        </div>
+      </Router>
+    )
+  }
 }
 
-export default App;
+export default App
